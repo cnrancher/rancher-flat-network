8828812ef12bdcdc58743b9419ea8332eb22a970
StarryWang <starry.wang@suse.com>
2024-07-09 17:50:42 +0800

    Add log output for hostname request

--- pkg/admission/ping.go
+++ pkg/admission/ping.go
@@ -4,18 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
 func pingHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("pong\n"))
 }
 
-func hostnameHandler(w http.ResponseWriter, _ *http.Request) {
+func hostnameHandler(w http.ResponseWriter, req *http.Request) {
 	n, err := os.Hostname()
 	if err != nil {
 		err := fmt.Errorf("failed to get hostname: %w", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	logrus.Infof("handle hostname request from [%v]", req.Host)
 	w.Write([]byte(n + "\n"))
 }
