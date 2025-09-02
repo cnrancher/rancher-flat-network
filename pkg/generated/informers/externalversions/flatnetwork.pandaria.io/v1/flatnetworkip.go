ce96e1ed36182462fcc274fb2c8d2effd7322fec
StarryWang <starry.wang@suse.com>
2025-09-02 11:33:43 +0800

    Bump dependencies

--- pkg/generated/informers/externalversions/flatnetwork.pandaria.io/v1/flatnetworkip.go
+++ pkg/generated/informers/externalversions/flatnetwork.pandaria.io/v1/flatnetworkip.go
@@ -62,13 +62,25 @@ func NewFilteredFlatNetworkIPInformer(client versioned.Interface, namespace stri
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.FlatnetworkV1().FlatNetworkIPs(namespace).List(context.TODO(), options)
+				return client.FlatnetworkV1().FlatNetworkIPs(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.FlatnetworkV1().FlatNetworkIPs(namespace).Watch(context.TODO(), options)
+				return client.FlatnetworkV1().FlatNetworkIPs(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.FlatnetworkV1().FlatNetworkIPs(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.FlatnetworkV1().FlatNetworkIPs(namespace).Watch(ctx, options)
 			},
 		},
 		&apisflatnetworkpandariaiov1.FlatNetworkIP{},
